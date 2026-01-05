f33c5c8c7bba4cd76b8485755db2a1bac9778ecb
Christopher Solanilla <chrissolanilla@gmail.com>
2026-01-04 22:53:52 -0500

    adds dpad ui

--- frontend/index.js
+++ frontend/index.js
@@ -13,7 +13,31 @@ const ctx = game.getContext("2d");
 console.log(ctx);
 //fps
 const FPS = 60;
-let dz = 0;
+let dz = 1;
+let angle = 0;
+
+//verticies
+const vs = [
+    {x: 0.25, y: 0.25, z: 0.25},
+    {x: -0.25, y: 0.25, z: 0.25},
+    {x: -0.25, y: -0.25, z: 0.25},
+    {x: 0.25, y: -0.25, z: 0.25},
+    //behind us
+    {x: 0.25, y: 0.25, z: -0.25},
+    {x: -0.25, y: 0.25, z: -0.25},
+    {x: -0.25, y: -0.25, z: -0.25},
+    {x: 0.25, y: -0.25, z: -0.25},
+]
+
+//faces
+const fs = [
+    [0,1,2,3],
+    [4,5,6,7],
+    [0,4],
+    [1,5],
+    [2,6],
+    [3,7],
+]
 
 //functions
 function clear() {
@@ -25,6 +49,15 @@ function point({x,y}) {
     const s = 20;
     ctx.fillStyle = fillColor;
     ctx.fillRect(x-s/2,y-s/2,s,s);
+}
+
+function line(p1,p2){
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = fillColor;
+    ctx.beginPath();
+    ctx.moveTo(p1.x,p1.y);
+    ctx.lineTo(p2.x,p2.y);
+    ctx.stroke();
 }
 
 function screen(p){
@@ -43,15 +76,47 @@ function project({x,y,z}){
     }
 }
 
+function translate_z({x,y,z}, dz){
+    return {x,y, z: z+dz};
+}
+
+function rotate_xz({x,y,z}, angle){
+    //rorate a vector formula:
+    //x`= xcos(theta) -ysin(theta)
+    //y` = xsin(theta) +ycos(theta)
+    const c = Math.cos(angle);
+    const s = Math.sin(angle)
+    return {
+        x: x*c-z*s,
+        y,
+        z: x*s+z*c,
+    };
+}
 
 function frame() {
     const dt = 1/FPS;
-    dz+=1*dt;
+    //dz+=1*dt;
+    //control speed by constant
+    angle += 1*Math.PI*dt;
+    //angle += 2*Math.PI*dt;
     clear()
-    point(screen(project({x: 0.5, y: 0.5, z: 1+dz})))
-    point(screen(project({x: -0.5, y: 0.5, z: 1+dz})))
-    point(screen(project({x: 0.5, y: -0.5, z: 1+dz})))
-    point(screen(project({x: -0.5, y: -0.5, z: 1+dz})))
+    //renders vertices, cooler if we dont
+    // for(const v of vs){
+    //     point(screen(project(translate_z(rotate_xz(v,angle), dz))))
+    // }
+
+    //iterate all faces connect current vertex and the other one
+    for(const f of fs){
+        for(let i =0;i<f.length;++i){
+            const a = vs[f[i]];
+            //this makes it so that if its the last index, it wraps back around to the first
+            const b = vs[f[(i+1)%f.length]];
+            line(
+                screen(project(translate_z(rotate_xz(a,angle),dz))),
+                screen(project(translate_z(rotate_xz(b,angle),dz)))
+            )
+        }
+    }
     //console.log(dz)
     setTimeout(frame, 1000/FPS);
 }
